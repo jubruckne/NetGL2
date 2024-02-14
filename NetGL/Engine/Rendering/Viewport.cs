2f2b1be2bd5dc4e3badd6cc157ed3739d4f17c69
Julia Bruckner <jubruckne@gmail.com>
2024-02-14 11:09:15 +0100

    .

--- NetGL/Engine/Rendering/Viewport.cs
+++ NetGL/Engine/Rendering/Viewport.cs
@@ -76,6 +76,7 @@ public class Viewport {
 
     public bool is_current => current_viewport == this;
 
+
     public override string ToString() {
         return $"{name}(x:{x}, y:{y}, w:{width}, h:{height}, color:{_clear_color}";
     }
