fb0db418f91b1c2471ae826e8f0292a99b12a558
Julia Bruckner <jubruckne@gmail.com>
2024-03-28 18:34:55 +0100

    .

--- NetGL/Engine/Disk/FileWriter.cs
+++ NetGL/Engine/Disk/FileWriter.cs
@@ -4,6 +4,6 @@ public static class FileWriter {
     public static void save_to_file<T>(this Field<T> field, string filename) where T: unmanaged {
         File.WriteAllText(AssetManager.temp_path(filename), "jjj");
 
-        Console.WriteLine($"{field} saved to {filename}.");
+        Console.WriteLine($"{field} saved to {filename}.");f
     }
 }
